9f90ff4fee75d86470e71ffc182f519b0afa16d1
Lucianor01 <lucianorosario02@gmail.com>
2023-07-03 15:37:38 +0200

    technologies filter

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Admin\Project;
+use App\Models\Admin\Technology;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -14,11 +15,31 @@ class ProjectController extends Controller
 
         // $projects = Project::with('type', 'technologies')->get();        
 
+        // if ($request->has('type_id')) {
+        //     $projects = Project::with('type', 'technologies')->where('type_id', $request->type_id)->paginate(4);
+        // } else {
+        //     $projects = Project::with('type', 'technologies')->paginate(4);
+        // }
+
+        // return response()->json([
+        //     'success' => true,
+        //     'projects' => $projects
+        // ]);
+
+        $query = Project::with(['type', 'technologies']);
+
         if ($request->has('type_id')) {
-            $projects = Project::with('type', 'technologies')->where('type_id', $request->type_id)->paginate(4);
-        } else {
-            $projects = Project::with('type', 'technologies')->paginate(4);
+            $query->where('type_id', $request->type_id);
         }
+
+        if ($request->has('technologies_ids')) {
+            $technologyIds = explode(',', $request->technologies_ids);
+            $query->whereHas('technologies', function ($query) use ($technologyIds) {
+                $query->whereIn('id', $technologyIds);
+            });
+        }
+
+        $projects = $query->paginate(4);
 
         return response()->json([
             'success' => true,
