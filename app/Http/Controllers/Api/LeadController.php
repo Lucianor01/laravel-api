8203ca149005919a3e3be592043884b5a3cd4d78
Lucianor01 <lucianorosario02@gmail.com>
2023-07-09 22:48:18 +0200

    LeadController

---
+++ app/Http/Controllers/Api/LeadController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Mail\NewContact;
+use App\Models\Admin\Lead;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class LeadController extends Controller
+{
+    public function store(Request $request)
+    {
+
+        $data = $request->all();
+
+        //? SALVATAGGIO DATI NEL DB
+        // $new_lead = new Lead();
+        // $new_lead->fill($data);
+        // $new_lead->save();
+
+        $new_lead = Lead::create($data);
+
+        //? INVIO DELLE MAIL
+        Mail::to('no-reply@boolpress.it')->send(new NewContact($new_lead));
+
+        //? OTTENERE UNA RISPOSTA POSITIVA IN JSON
+        return response()->json(
+            [
+                'success' => true
+            ]
+        );
+    }
+}
