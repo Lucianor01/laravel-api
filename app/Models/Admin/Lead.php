3573d8868c1ddf58e5f522913d85f26459f30d20
Lucianor01 <lucianorosario02@gmail.com>
2023-07-09 22:34:53 +0200

    Creazione cartella mail

--- app/Models/Admin/Lead.php
+++ app/Models/Admin/Lead.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Lead extends Model
 {
     use HasFactory;
+
+    protected $table = 'leads';
+
+    protected $fillable = ['name', 'email', 'message'];
 }
